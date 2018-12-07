9755844efe7f26ba5622439ec85b5889c17acda7
Luiza2 <43903275+Luiza2@users.noreply.github.com>
2018-12-07 17:18:44 +0100

    Add files via upload

--- projekt/src/projekt/Sumy.java
+++ projekt/src/projekt/Sumy.java
@@ -6,15 +6,19 @@ import javax.swing.JLabel;
 
 public class Sumy extends JLabel {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String sumaLiczbString = " ";
 	Sumy(int i, int sumaLiczb){
 		
-		//this.i = i;
-		this.setBounds(90 ,i ,100 ,100);
+		this.setBounds(90 ,i ,70 ,70);
 		Font czcionka = new Font("Italic", 0, 24);
 		sumaLiczbString = Integer.toString(sumaLiczb);
 		this.setFont(czcionka);
 		this.setText(sumaLiczbString);
+		
 	}
 	
 }
