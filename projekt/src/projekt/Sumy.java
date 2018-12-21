1ccb594b528f595a143de6b01c19b52b4b2c9529
Luiza2 <43903275+Luiza2@users.noreply.github.com>
2018-12-21 12:15:49 +0100

    Add files via upload

--- projekt/src/projekt/Sumy.java
+++ projekt/src/projekt/Sumy.java
@@ -1,24 +1,43 @@
 package projekt;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JLabel;
 
-public class Sumy extends JLabel {
+public class Sumy extends JLabel implements ActionListener {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private SumyKontroler kontroler;
+	Color kolorPoprawny = new Color(135,	206	,235);
 	String sumaLiczbString = " ";
-	Sumy(int i, int sumaLiczb){
+	Sumy(SumyKontroler kontroler, int i, int sumaLiczb){
 		
 		this.setBounds(90 ,i ,70 ,70);
 		Font czcionka = new Font("Italic", 0, 24);
 		sumaLiczbString = Integer.toString(sumaLiczb);
 		this.setFont(czcionka);
 		this.setText(sumaLiczbString);
+		this.kontroler = kontroler;
+	}
+	
+	public void zmienKolor() {
 		
+		Font czcionka1 = new Font("Italic", 0, 30);
+		this.setForeground(kolorPoprawny);
+		//this.setFont(czcionka1);
+		//System.out.println("zmieniarka koloru z klasy sumy");
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		//zmienKolor();
 	}
 	
 }
---
+++ projekt/src/projekt/SumyKontroler.java
@@ -0,0 +1,13 @@
+package projekt;
+
+public class SumyKontroler {
+
+	
+	public SumyKontroler() {
+		
+	}
+	
+	public void zmienKolor() {
+		
+	}
+}
